a8f22df22f39aa1d51d2e26cbdd28cb88477f07f
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-11-22 15:15:12 -0500

    new color

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -2,7 +2,8 @@ const colors = {
   dark: "#181820",
   lightDark: "#21212B",
   grey: "#606060",
-  lightWhite: "#fafafa",
+  lightWhite: "#FAFAFA",
+  desacGrey: "#B2B2B5",
 
   colorsButtons: {},
 };
