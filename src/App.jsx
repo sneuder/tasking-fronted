2b59e88aeb5ae4bfaa2338a5ab2d0c2d57e42053
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-11-24 16:34:15 -0500

    context collection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import PublicRoutes from "../src/routes/public.routes";
 import GlobalStyle from "./styles/globalStyle";
 
+import CollectionsContext from "./data/context/collections/collectionsContext";
+import CollectionsUpdater from "./data/context/collections/collectionsUpdater";
+
 function App() {
   return (
     <>
       <GlobalStyle />
-      <PublicRoutes />
+      <CollectionsContext.Provider value={CollectionsUpdater()}>
+        <PublicRoutes />
+      </CollectionsContext.Provider>
     </>
   );
 }
