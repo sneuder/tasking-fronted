cfd6fb2983e30582ebd8554b7a41f58edcaf9f59
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-11-24 21:52:18 -0500

    chage due to localStorage

--- src/data/models/collectionModel.js
+++ src/data/models/collectionModel.js
@@ -1,22 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
-import TaskModel from "./taskModel";
 
-function CollectionModel(collectionName) {
-  const id = uuidv4();
-  let name = collectionName;
-  const tasks = [];
-
-  this.getIdCollection = () => {
-    return id;
-  };
-
-  this.addTask = (description) => {
-    const task = new TaskModel(uuidv4(), description);
-  };
-
-  this.removeTask = (taskId) => {
-    tasks = tasks.filter((task) => task !== taskId);
-  };
+class CollectionModel {
+  constructor(collectionName) {
+    this.id = uuidv4();
+    this.name = collectionName;
+    this.tasks = [];
+  }
 }
 
 export default CollectionModel;
--- src/data/models/taskModel.js
+++ src/data/models/taskModel.js
@@ -1,44 +1,13 @@
-function TaskModel(idTask, descriptionTask) {
-  const id = idTask;
-  let description = descriptionTask;
-  let completed = false;
+import { v4 as uuidv4 } from "uuid";
 
-  const subTasks = [];
-  const completedSubTasks = [];
-
-  this.getTaskId = () => {
-    return id;
-  };
-
-  this.getDescription = () => {
-    return description;
-  };
-
-  this.completeTask = () => {
-    completed = true;
-  };
-
-  this.uncompleteTask = () => {
-    completed = false;
-  };
-
-  this.addSubtask = (id, description) => {
-    const subTask = new TaskModel(id, description);
-    subTasks.shift(subTask);
-  };
-
-  this.removeSubtask = (idSubTask) => {
-    subTasks = subTasks.filter((subTask) => subTask.getTaskId() !== idSubTask);
-  };
-
-  this.completeSubTask = (idSubtask) => {
-    subTasks = this.subTasks.map((subTask) => {
-      if (subTask.getTaskId === idSubtask) {
-        subTask.completeTask();
-        completedSubTasks.push();
-      }
-    });
-  };
+class TaskModel {
+  constructor(descriptionTask) {
+    this.id = uuidv4();
+    this.description = descriptionTask;
+    this.completed = false;
+    this.subTasks = [];
+    this.completedSubTasks = [];
+  }
 }
 
 export default TaskModel;
