8a6943c50d92a86c596f38fa005b5ab4e7eb53c5
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-11-25 09:02:11 -0500

    basics for react hook form

--- src/components/AddTaskBar/index.jsx
+++ src/components/AddTaskBar/index.jsx
@@ -1,11 +1,17 @@
 import AddTaskButton from "../../shared/AddTaskButton/index";
 import { AddTaskWrapper, InputTask } from "./elements";
 
+// test
+import useCollection from "../../hooks/useCollection";
+import useTask from "../../hooks/useTask";
+
 const AddTaskBar = () => {
+  const { handleSubmit, handleTask, register } = useTask();
+
   return (
-    <AddTaskWrapper>
+    <AddTaskWrapper onSubmit={handleSubmit(handleTask)}>
       <AddTaskButton />
-      <InputTask placeholder="Add a task..." />
+      <InputTask placeholder="Add a task..." {...register("taskDescription")} />
     </AddTaskWrapper>
   );
 };
--- src/hooks/useTask.jsx
+++ src/hooks/useTask.jsx
@@ -1,20 +1,24 @@
 import { useContext } from "react";
+import { useForm } from "react-hook-form";
+
 import CollectionsContext from "../data/context/collections/collectionsContext";
 import TaskModel from "../data/models/taskModel";
 
 const useTask = (oldCollection) => {
+  const { register, handleSubmit } = useForm();
   const { handleSetTask } = useContext(CollectionsContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // implement React Hook Form
-    const newTask = new TaskModel("description");
-    handleSetTask(oldCollection, newTask);
+  const handleTask = (data) => {
+    const { taskDescription } = data;
+    const newTask = new TaskModel(taskDescription);
+    console.log(newTask);
+    // handleSetTask(oldCollection, newTask);
   };
 
   return {
+    handleTask,
     handleSubmit,
+    register,
   };
 };
 
